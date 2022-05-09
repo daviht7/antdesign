73f285ebdd54d641efb9d7047e0742989f2a7644
Davi Holanda <daviht7@gmail.com>
2022-05-09 18:57:24 -0300

    Responsividade com Breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import './index.css';
 
 function App() {
   return (
-    <Row gutter={10} justify={'center'} align={'middle'}>
-      <Col className='Column' span={3}>
+    <Row gutter={8} justify={'center'}>
+      <Col
+        xs={24}
+        sm={12}
+        md={12}
+        lg={8}
+        className='Column'
+      >
+        ok
+      </Col>
+      {/* <Col className='Column' span={3}>
         ok
       </Col>
       <Col className='Column' span={3}>
@@ -12,10 +21,7 @@ function App() {
       </Col>
       <Col className='Column' span={3}>
         ok
-      </Col>
-      <Col className='Column' span={3}>
-        ok
-      </Col>
+      </Col> */}
     </Row>
   );
 }
