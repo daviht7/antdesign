8cc16a5f6a8089a0d1a38e00831c59bd18bebdd4
Davi Holanda <daviht7@gmail.com>
2022-05-09 18:15:02 -0300

    Introdução ao Grid (Row e Col)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { DatePicker } from "antd";
-import locale from "antd/lib/date-picker/locale/pt_BR";
+import { Col, Row } from 'antd';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <DatePicker locale={locale} />
+    <div>
+      <Row>
+        <Col span={4} className='Column'>
+          coluna 1
+        </Col>
+        <Col span={4} className='Column'>
+          22222222222222222222222222222222222222222222222222222222222222222222
+        </Col>
+        <Col span={4} className='Column'>
+          coluna 3
+        </Col>
+      </Row>
     </div>
   );
 }
