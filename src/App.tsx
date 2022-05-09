ff7b73337c10ecfa74f7121a976c550f48a089c2
Davi Holanda <daviht7@gmail.com>
2022-05-09 17:52:55 -0300

    limpando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { DatePicker } from 'antd';
-import locale from 'antd/lib/date-picker/locale/pt_BR';
+import { DatePicker } from "antd";
+import locale from "antd/lib/date-picker/locale/pt_BR";
 
 function App() {
   return (
