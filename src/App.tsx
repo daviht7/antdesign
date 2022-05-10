50bb2ee43c903f297fa1c945a626c3b30e33d0e1
Davi Holanda <daviht7@gmail.com>
2022-05-10 18:52:59 -0300

    Tipografia

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import './index.css';
 
 function App() {
   return (
     <Row gutter={8} justify={'center'}>
-      <Col
-        xs={24}
-        sm={12}
-        md={12}
-        lg={8}
-        className='Column'
-      >
-        ok
+      <Col span={12}>
+        <Typography.Title level={1}>
+          nivel 1
+        </Typography.Title>
+        <Typography.Title level={2}>
+          nivel 2
+        </Typography.Title>
+        <Typography.Title level={3}>
+          nivel 3
+        </Typography.Title>
+        <Typography.Title level={4}>
+          nivel 4
+        </Typography.Title>
+        <Typography.Title level={5}>
+          nivel 5
+        </Typography.Title>
       </Col>
-      {/* <Col className='Column' span={3}>
-        ok
-      </Col>
-      <Col className='Column' span={3}>
-        ok
-      </Col>
-      <Col className='Column' span={3}>
-        ok
-      </Col> */}
     </Row>
   );
 }
