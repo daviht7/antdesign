8f072c8acb17c25c4e87130a5bddba2ec21c13a6
Davi Holanda <daviht7@gmail.com>
2022-05-10 19:04:52 -0300

    Adicionando e entendendo o Layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,49 @@
-import "antd/dist/antd.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { Breadcrumb, Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const { Header, Footer, Content } = Layout;
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout className='layout'>
+      <Header>
+        <div className='logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['2']}
+          items={new Array(15)
+            .fill(null)
+            .map((_, index) => {
+              const key = index + 1;
+              return {
+                key,
+                label: `nav ${key}`,
+              };
+            })}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='site-layout-content'>
+          <App />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   </React.StrictMode>
 );
 
