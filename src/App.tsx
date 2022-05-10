861e6b733655cac6bad4818447eb3fe714409989
Davi Holanda <daviht7@gmail.com>
2022-05-10 19:58:07 -0300

    typescript-eslint\no-unused-vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { Col, Input, Row, Table } from 'antd';
+import {
+  Button,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  Row,
+  Table,
+} from 'antd';
 import { User, UserService } from 'daviht7-sdk';
 import { useEffect, useState } from 'react';
 import './index.css';
@@ -28,11 +36,66 @@ function App() {
     UserService.getAllUsers().then(setUsers);
   }, []);
 
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <Row gutter={8} justify={'center'}>
+    <Row gutter={32} justify={'center'}>
       <Col span={12}>
-        <Input placeholder='Basic usage' maxLength={10} />
-        <Table dataSource={users} columns={columns} />;
+        <Form
+          name='basic'
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          autoComplete={'off'}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label='Username'
+            name='username'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input autoComplete={'off'} />
+          </Form.Item>
+
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name='remember'
+            valuePropName='checked'
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type='primary' htmlType='submit'>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </Col>
     </Row>
   );
