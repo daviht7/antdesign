1ed6f27e8bcecb1b8e4bbdf9ed492c7e50627e79
Davi Holanda <daviht7@gmail.com>
2022-05-10 19:21:39 -0300

    Introdução ao Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import { Col, Row, Typography } from 'antd';
+import { Col, Row, Table } from 'antd';
+import { User, UserService } from 'daviht7-sdk';
+import { useEffect, useState } from 'react';
 import './index.css';
 
+const columns = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+];
+
 function App() {
+  const [users, setUsers] = useState<User.Summary[]>([]);
+
+  useEffect(() => {
+    UserService.getAllUsers().then(setUsers);
+  }, []);
+
   return (
     <Row gutter={8} justify={'center'}>
       <Col span={12}>
-        <Typography.Title level={1}>
-          nivel 1
-        </Typography.Title>
-        <Typography.Title level={2}>
-          nivel 2
-        </Typography.Title>
-        <Typography.Title level={3}>
-          nivel 3
-        </Typography.Title>
-        <Typography.Title level={4}>
-          nivel 4
-        </Typography.Title>
-        <Typography.Title level={5}>
-          nivel 5
-        </Typography.Title>
+        <Table dataSource={users} columns={columns} />;
       </Col>
     </Row>
   );
